e5ac74b5c518e7d9ee166cb2cb8fa813a7eac2fa
cirkuit32 <emilianorottaf@gmail.com>
2022-08-08 15:36:48 -0300

    rutas del get, post funcionando desde el back

--- api/src/db.js
+++ api/src/db.js
@@ -30,14 +30,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Cursos } = sequelize.models;
-
-// Aca vendrian las relaciones
-// Product.hasMany(Reviews);
-
-// Cursos, {through: cursos};
-
-
+// const { Cursos } = sequelize.models;
 
 
 module.exports = {
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const {Cursos} = require ("../db")
+const {Op} = require('sequelize')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -9,39 +10,73 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+
+//busco los cursos por el nombre
+const nombreBaseDatos = async (i) => {
+  try {
+    let cursos = await Cursos.findAll({
+      where: {nombre : {[Op.iLike]:`%${i}%`}}
+    })
+    console.log (cursos)
+    return cursos
+        } catch (error) {
+          console.log("Hubo un error en nombreBaseDatos", error)
+        }
+      };
+
+router.get ("/curso", async (req, res)=>{
+  try {
+    const nombre = req.query.nombre;
+    const cursosBD = await nombreBaseDatos (nombre);
+    res.send (cursosBD)
+    } 
+    catch (error) {
+    console.log("Se encontro una falla en el get nombreBaseDatos /cursos ", error)
+  }
+});
+
 //llamado a la base de datos para trerme los cursos
-const baseDatos = async () => {
+const todoBaseDatos = async (i) => {
     try {
-      return await Cursos
+      let cursos = await Cursos.findAll()
+      console.log (cursos)
+      return cursos
           } catch (error) {
-            console.log("Hubo un error en baseDatos", error)
+            console.log("Hubo un error en todoBaseDatos", error)
           }
         };
 
-
-//busco los cursos por el nombre
+        
 router.get ("/cursos", async (req, res)=>{
-    try {
-      const nombre = req.query.nombre;
-      const cursosBD = await baseDatos ();
-      if (nombre){
-          let nombreCurso = await cursosBD.filter(e => e.nombre.toLowerCase().includes(nombre.toLowerCase()));
-          nombreCurso.length ?
-          res.status(200).send(nombreCurso):
-          res.status(404).send("No se encontro el curso");
-      }else{
-          res.status(200).send(cursosBD)
-      }
-      
-    } catch (error) {
-      console.log("Se encontro una falla en el get /cursos", error)
-    }
-  });
+  try {
+    const nombre = req.query.nombre;
+    const cursosBD = await todoBaseDatos (nombre);
+    res.send (cursosBD)
+    } 
+    catch (error) {
+    console.log("Se encontro una falla en el get todoBaseDatos /cursos ", error)
+  }
+});
+
+
+
   
+  // un get para buscar por ID, para ver el detalle
+  router.get("/idCurso/:id", async (req,res)=>{
+    const id = req.params.id ;
+    const cursosBD = await todoBaseDatos ();
+    if (id){
+        let idCurso = await cursosBD.filter(e => e.id == id)
+        idCurso.length? 
+        res.status (200).json (idCurso):
+        res.status (404).send ("No se encontro el curso por ID")
+    }
+  })
 
 
 //Crear cursos (con la opcion deshabilitar, se oculta el curso, de esta manera no hace falta eliminarlo)
-  router.post('/curso', async (req, res) => {
+  
+router.post('/curso', async (req, res) => {
     
   
     const { nombre, imagen, description, deshabilitar} = req.body
@@ -63,8 +98,28 @@ catch(error){
 }
 ); 
 
+router.put('/editar/:id', async (req, res) => {
+  const id = req.params.id ;
+  const cursos = req.body
+
+  try {
+      await Cursos.update(Cursos, ({
+              where: {
+                  id: id
+              }
+          }))
+      
+      if(cursos.deshabilitar === true){
+          res.send(`Producto Deshabilitado`)
+      }else{
+          res.send(`Producto actualizado`)
+      }
+  } catch (error) {
+      res.status(404).send("No se pudo actualizar el producto")
+  }
 
 
-
+}
+);
 
 module.exports = router;
